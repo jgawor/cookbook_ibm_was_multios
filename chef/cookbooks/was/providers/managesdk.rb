19e64be93314e1a95cde062352c48d362c3faea2
Jarek Gawor <jgawor@gmail.com>
2019-06-24 13:52:15 -0400

    more 9.0.5 fixes

--- chef/cookbooks/was/providers/managesdk.rb
+++ chef/cookbooks/was/providers/managesdk.rb
@@ -15,7 +15,7 @@ action :setCommandDefault do
       when 'linux'
         sdk_version = getsdkversion
         log "sdk_version: #{sdk_version}"
-        cmd = "#{new_resource.install_dir}/bin/managesdk.sh -setCommandDefault -sdkname #{sdk_version}"
+        cmd = "USER_INSTALL_ROOT='#{new_resource.install_dir}' #{new_resource.install_dir}/bin/managesdk.sh -setCommandDefault -sdkname #{sdk_version}"
         log cmd
         cmd_out = run_shell_cmd(cmd, new_resource.admin_user)
         log cmd_out.stdout
@@ -33,7 +33,7 @@ action :setNewProfileDefault do
       when 'linux'
         sdk_version = getsdkversion
         log "sdk_version: #{sdk_version}"
-        cmd = "#{new_resource.install_dir}/bin/managesdk.sh -setNewProfileDefault -sdkname #{sdk_version}"
+        cmd = "USER_INSTALL_ROOT='#{new_resource.install_dir}' #{new_resource.install_dir}/bin/managesdk.sh -setNewProfileDefault -sdkname #{sdk_version}"
         log cmd
         cmd_out = run_shell_cmd(cmd, new_resource.admin_user)
         log cmd_out.stdout
@@ -58,7 +58,7 @@ def getsdkversion
   when 'linux'
     java_v = new_resource.java_version.split('.')[0] + '.' + new_resource.java_version.split('.')[1]
     Chef::Log.debug("#{new_resource.install_dir}/bin/managesdk.sh -listAvailable  | grep #{java_v}")
-    cmd_available = "#{new_resource.install_dir}/bin/managesdk.sh -listAvailable | grep #{java_v}"
+    cmd_available = "USER_INSTALL_ROOT='#{new_resource.install_dir}' #{new_resource.install_dir}/bin/managesdk.sh -listAvailable | grep #{java_v}"
     cmd_available_out = run_shell_cmd(cmd_available, new_resource.admin_user)
     sdk_version = cmd_available_out.stdout.split[-1]
     Chef::Log.info("sdk_version: #{sdk_version}")
@@ -69,7 +69,7 @@ def check_sdk?(option)
   case node['os']
   when 'linux'
     sdk_version = getsdkversion
-    cmd_check_sdk = "#{new_resource.install_dir}/bin/managesdk.sh -#{option}"
+    cmd_check_sdk = "USER_INSTALL_ROOT='#{new_resource.install_dir}' #{new_resource.install_dir}/bin/managesdk.sh -#{option}"
   end
   cmd = run_shell_cmd(cmd_check_sdk, @new_resource.admin_user)
   cmd.stderr.empty? && /#{sdk_version}/.match(cmd.stdout)
